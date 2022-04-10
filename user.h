705e6e3a1f80a75fcb2ff9d1d1924ae1c2829287
Luke <56962839+Luke-Sanmoogan@users.noreply.github.com>
2022-04-10 20:30:45 +0100

    add new system calls to user.h

--- user.h
+++ user.h
@@ -23,6 +23,9 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
+// Newly added system calls for assignment
+int mprotect(void *addr, int len);
+int munprotect(void *addr, int len);
 
 // ulib.c
 int stat(const char*, struct stat*);
